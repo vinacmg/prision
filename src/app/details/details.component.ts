9c7efa895f8b76da6b1c75dfc28f620db8c15a52
Vinícius <viniciuscmgo@gmail.com>
2018-06-21 22:35:55 -0300

    iniciado serviço de detalhes

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { ItemsService } from '../items/items.service';
+import { DetailsService } from './details.service';
 
 @Component({
   selector: 'app-details',
@@ -17,8 +18,20 @@ export class DetailsComponent implements OnInit {
   type:string;
   obj:any[];
   value = 'um valor aí';
+  bloco = { numero:0, andar:0 };
+  cela = { codigo:0, quantidade_max:0, tipo:''};
+  familiar = { cpf:'', data_nascimento: '', rg:'', nome:'', parentesco:'' };
+  fornecedor = { cnpj:'', nome_empresa:'', item_ofertado:'' };
+  pavilhao = { numero:0, funcao:'' };
+  servidor = { cpf:'', data_nascimento:'', cargo:'', nome:'', salario:0 };
+  unidade = { codigo:0, nome:'', rua:'', bairro:'', cidade:'', estado:'', cep:'' };
+  prisioneiro = { cpf:'', data_nascimento:'', observacoes_medicas:'', nome:'', rg:'' };
+  pena = { codigo_penal:'', area_judicial:'', descricao:'', duracao_min:0, duracao_max:0 };
 
-  constructor(private itemsService: ItemsService) { }
+  constructor(
+    private itemsService: ItemsService,
+    private detailsService: DetailsService
+  ) { }
 
   ngOnInit() {
     this.itemsService.change.subscribe(type => {
@@ -28,6 +41,7 @@ export class DetailsComponent implements OnInit {
 
   set(key:any){ //usar details service pra dar get nessa key aqui dentro
     this.name = key;
+    /*
     let obj = {
       chave: 'artigo_penal',
       artigo_penal:555,
@@ -38,33 +52,37 @@ export class DetailsComponent implements OnInit {
     };
     this.keys = Object.keys(obj);
     this.values = Object.values(obj);
-    /*
-    switch(type) {
+    */
+    switch(this.type) {
       case 'unidades':
+        this.unidade = this.detailsService.get('teste');
         break;
       case 'fornecedores':
+        this.fornecedor = this.detailsService.get('teste');
         break;
       case 'pavilhões':
+        this.pavilhao = this.detailsService.get('teste');
         break;
       case 'blocos':
+        this.bloco = this.detailsService.get('teste');
         break;
       case 'celas':
+        this.cela = this.detailsService.get('teste');
         break;
       case 'prisioneiros':
+        this.prisioneiro = this.detailsService.get('teste');
         break;
       case 'familiares':
+        this.familiar = this.detailsService.get('teste');
         break;
       case 'servidores':
+        this.servidor = this.detailsService.get('teste');
         break;
       case 'penas':
-        //pega dados do banco
-        //apresenta na tela adequadamente
-        this.keys = Object.keys(obj);
-        this.values = Object.values(obj);
+        this.pena = this.detailsService.get('teste');
         break;            
       default:
     }
-    */
   }
 
   setAddingItem(value: boolean) {
@@ -81,6 +99,19 @@ export class DetailsComponent implements OnInit {
 
   fornecedoresByUnidade(codigo) {
     this.itemsService.fornecedoresByUnidade(codigo);
+  }
+
+  save() {
+    //coloca no banco e 
+    this.bloco = { numero:0, andar:0 };
+    this.cela = { codigo:0, quantidade_max:0, tipo:''};
+    this.familiar = { cpf:'', data_nascimento: '', rg:'', nome:'', parentesco:'' };
+    this.fornecedor = { cnpj:'', nome_empresa:'', item_ofertado:'' };
+    this.pavilhao = { numero:0, funcao:'' };
+    this.servidor = { cpf:'', data_nascimento:'', cargo:'', nome:'', salario:0 };
+    this.unidade = { codigo:0, nome:'', rua:'', bairro:'', cidade:'', estado:'', cep:'' };
+    this.prisioneiro = { cpf:'', data_nascimento:'', observacoes_medicas:'', nome:'', rg:'' };
+    this.pena = { codigo_penal:'', area_judicial:'', descricao:'', duracao_min:0, duracao_max:0 };
   }
 
 }
--- src/app/details/details.service.ts
+++ src/app/details/details.service.ts
@@ -1,12 +1,52 @@
 import { Injectable, Output, EventEmitter } from "@angular/core";
+import { ItemsService } from "../items/items.service";
 
 @Injectable()
 export class DetailsService {
 
   @Output() detailsChanges: EventEmitter<any> = new EventEmitter();
 
-  items:any;
+  details:any;
+  type:string;
 
-  constructor() { }
+  constructor(private itemsService: ItemsService) { 
+    this.itemsService.change.subscribe(type => {
+      this.type = type;
+    });
+  }
+
+  get(key:any) {
+    switch(this.type) {
+      case 'unidades':
+        this.details = { codigo:10, nome:'hsud', rua:'asds', bairro:'asd', cidade:'asdsd', estado:'eew', cep:'1234' };
+        break;
+      case 'fornecedores':
+        this.details = { cnpj:'10', nome_empresa:'123123', item_ofertado:'ddsda' };
+        break;
+      case 'pavilhões':
+        this.details = { numero:10, funcao:'123213' };
+        break;
+      case 'blocos':
+        this.details = { numero:10, andar:1 };
+        break;
+      case 'celas':
+        this.details = { codigo:10, quantidade_max:3, tipo:'rew'};
+        break;
+      case 'prisioneiros':
+        this.details = { cpf:'10', data_nascimento:'gfd', observacoes_medicas:'ertert', nome:'weqwe', rg:'vcsdfd' };
+        break;
+      case 'familiares':
+        this.details = { cpf:'10', data_nascimento: 'qaxcss', rg:'ybhg', nome:'dcsf', parentesco:'vfvf' };
+        break;
+      case 'servidores':
+        this.details = { cpf:'10', data_nascimento:'rertrtr', cargo:'sadasd', nome:'asd', salario:322 };;
+        break;
+      case 'penas':
+        this.details = { codigo_penal:'10', area_judicial:'4545', descricao:'sadas', duracao_min:1, duracao_max:2 };
+        break;            
+      default:
+    }
+    return this.details;
+  }
 
 }
